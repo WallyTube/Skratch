ab418f2dd25e9649731ffcd1edd15237ce7428d0
WallyTube <wolfy9917@gmail.com>
2023-03-08 17:38:12 -0600

    Added a mod icon, owo-lib dependency, and some simple keybind functionality.

--- src/main/java/me/wally/skratch/client/SkratchClient.java
+++ src/main/java/me/wally/skratch/client/SkratchClient.java
@@ -13,11 +13,11 @@ public class SkratchClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
-        KeyBinding binding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.test_keybinding_1", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_P, "key.category.first.test"));
+        KeyBinding menuBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("Skratch Menu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M, "Skratch"));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (binding1.wasPressed()) {
-                client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
+            if (menuBind.wasPressed()) {
+                client.player.sendMessage(Text.literal("Menu bind was pressed!"), false);
             }
         });
 
